d4b100c6ba00df881d16dba47d8cd305bbcb4600
Appala Raju <raju@crypsis.net>
2016-12-23 12:17:50 +0530

    Dev commit added

--- Sample/ViewController.m
+++ Sample/ViewController.m
@@ -21,6 +21,8 @@
     UITextField *passwordTextField;
     NSString *macid = @"";
     
+    //This is dev branch
+    
     NSURL *url = [NSURL URLWithString:[[NSString stringWithFormat:@"http://43.241.67.139:390/api/Login/verifyLoginCredentials?Email=%@&Password=%@&MacId=%@",emailTextField.text,passwordTextField.text,macid]stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]]];
     
     // Do any additional setup after loading the view, typically from a nib.
